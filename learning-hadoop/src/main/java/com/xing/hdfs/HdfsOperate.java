6606c10dcbc37b9351a64509a6b3eabfb840daea
xingguo <xingguo0828@gmail.com>
2022-03-27 21:02:16 +0800

    [update]add note

--- learning-hadoop/src/main/java/com/xing/hdfs/HdfsOperate.java
+++ learning-hadoop/src/main/java/com/xing/hdfs/HdfsOperate.java
@@ -11,6 +11,8 @@ import java.net.URISyntaxException;
 
 /**
  * hdfs操作相关
+ * WARN[org.apache.hadoop.util.NativeCodeLoader]-Unable to load native-hadoop library for your platform... using builtin-java classes where applicable
+ * 没有影响
  *
  * @author xingguo
  */
@@ -18,10 +20,19 @@ public class HdfsOperate {
     public static void main(String[] args) throws URISyntaxException, IOException, InterruptedException {
         //1获取文件系统
         Configuration configuration = new Configuration();
-        //配置在集群上运行
+        //配置在集群上运行，设置hdfs 协议 配置自定义hadoop集群namenode地址
+        // 集群namenode地址查看 cat $HADOOP_HOME/etc/hadoop/core-site.xml 中 fs.defaultFS 配置
+        // 对于hadoop101域名需要在本地 /etc/hosts中配置 域名对应的ip
         FileSystem fs = FileSystem.get(new URI("hdfs://hadoop101:9000"), configuration, "hadoop");
         //2创建目录
-        fs.mkdirs(new Path("/test/local/client"));
+        boolean exists = fs.exists(new Path("/test"));
+        if (exists) {
+            fs.deleteOnExit(new Path("/test"));
+            System.out.println("删除");
+        } else {
+            fs.mkdirs(new Path("/test/local/client"));
+            System.out.println("新增");
+        }
         //3关闭资源
         fs.close();
     }
